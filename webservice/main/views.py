da18c7ca976fc5b65b940548e639dda63bb62b36
SM0487 <msa0487@yandex.ru>
2023-06-10 00:40:23 +0300

    1) при отсутствии реальных данных (сервис был отклчен) заполняем промежутки 0 значениями; 2) убрал циклы, и через функции pandas реализовал срезы необходимых данных. Вычисления среднего делал без учета заполненных 0-х значений (т.е. когда сервис не работал), иначе значения были бы не корректными. 3) поправил ошибку, возникающую при пустом списке значений. 4) добавил пару новых скриншотов для сравнения.

--- webservice/main/views.py
+++ webservice/main/views.py
@@ -6,7 +6,6 @@ import pandas as pd
 from plotly.offline import plot
 import plotly.express as px
 
-
 # Create your views here.
 # функция построения графиков, возвращает словарь с HTML-кодами графиков
 def view_graph():
@@ -22,65 +21,59 @@ def view_graph():
 
         # подготавливаем список данных для экспорта в график
         if len(qs) != 0:
-            stat_data_moment = [
+            stat_data = [
                 {
                     'DateTime': elem.date_time,
                     'Loading, %': elem.cpu_loading
                 } for elem in qs
             ]
         else:
-            stat_data_moment = []
+            # добавляем в список данных начальную и конечную точки графика, иначе будет ошибка если данных нет
+            stat_data = [{'DateTime': filter_dt_start, 'Loading, %': 0},
+                         {'DateTime': filter_dt_finish, 'Loading, %': 0}]
 
-        # добавляем в список данных начальную и конечную точки графика
-        #stat_data_moment.append({'DateTime': filter_dt_start, 'Loading, %': 0})
-        #stat_data_moment.append({'DateTime': filter_dt_finish, 'Loading, %': 0})
-
-        # структурируем данные для первого графика при помощи pandas.dataframe
-        df_plot1 = pd.DataFrame(stat_data_moment)
+        # --> формируем таблицу 5 секундных данных, где 0-значения не работающего сервиса
+        # преобразуем список в DataFrame, исходную таблицу, для дальнейшей обработки
+        df5 = pd.DataFrame(stat_data).sort_values('DateTime')
+        # генерируем таблицу 5 секундных интервалов в указанном промежутке заполненную 0
+        df0 = pd.DataFrame({"DateTime": pd.date_range(filter_dt_start, filter_dt_finish, freq="5S"), 'Loading, %': 0})
+        df_5sec = pd.concat([df0, df5])  # объединяем данные 2-х таблиц
+        df_5sec = df_5sec.sort_values('DateTime')  # сортируем по ДатеВремени
+        df_5sec['_DateTime'] = df_5sec['DateTime']  # добавляем столбец для объединения в 5сек интервалы
+        stat_data_moment = df_5sec.resample('5S', on='_DateTime').max()  # заполняем интервалы реальными значениями
+        # <-- конец, формируем таблицу 5 секундных данных, где 0-значения не работающего сервиса
 
         # формируем первый график, из ранее подготовленных данных, в виде гистограммы
         plot1 = px.bar(
-            df_plot1,
+            stat_data_moment,
             title='Активность ЦП за последний час, каждые 5 сек.',
             x="DateTime",
             y="Loading, %",
             range_x=[filter_dt_start, filter_dt_finish],
         )
-        plot1.update_traces(marker_color='#00CC66')
+        plot1.update_traces(marker_color='#00CC66')  # указывам цвет столбцов графика
 
-        # --> вычисление среднего за каждую минуту и заполнение списка значений
-        stat_data_mid = []  # список словарей
-        _filter_interval = [filter_dt_start, filter_dt_start]  # временный фильтр для 5 секундного интервала [начало, конец]
-        # в цикле анализируем значения в stat_data_moment
-        while _filter_interval[1] < filter_dt_finish:
-            # меняем конец интервала, увеличиваем на 1 минуту
-            _filter_interval[1] = _filter_interval[0] + datetime.timedelta(minutes=1)
-            _sum = 0  # временная переменная для суммы значений, для вычесления среднего
-            _count = 0  # временная переменная для количества просуммированных значений, для вычесления среднего
-            # проходимся по списку исходных значений
-            for element in stat_data_moment:
-                # выбираем значения попадающие в заданный интервал
-                if _filter_interval[0] <= element["DateTime"] < _filter_interval[1]:
-                    _count += 1
-                    _sum += element["Loading, %"]
-            # добавляем результаты вычислений по отфильтрованным значениям в список
-            stat_data_mid.append({'DateTime': _filter_interval[1], 'Loading, %': (_sum / _count if _count != 0 else 0)})
-            # меняем начало интервала
-            _filter_interval[0] = _filter_interval[1]
-        # <-- конец, вычисление среднего за каждую минуту и заполнение списка значений
-
-        # структурируем данные для второго графика при помощи pandas.dataframe
-        df_plot2 = pd.DataFrame(stat_data_mid)
+        # --> формируем таблицу с поминутными средними значениями, где 0-значения не работающего сервиса
+        df_mean = df5.copy()  # копируем исходную таблицу
+        df_mean['_DateTime'] = df_mean['DateTime']  # добавляем столбец для объединения в 1мин интервалы
+        df_mean = df_mean.resample('1T', on='_DateTime').mean()  # заполняем интервалы средними значениями
+        # генерируем таблицу 1 минутных интервалов в указанном промежутке заполненную 0
+        df0 = pd.DataFrame({"DateTime": pd.date_range(filter_dt_start, filter_dt_finish, freq="1T"), 'Loading, %': 0})
+        df_mean = pd.concat([df0, df_mean])  # объединяем данные 2-х таблиц
+        df_mean = df_mean.sort_values('DateTime')  # сортируем по ДатеВремени
+        df_mean['_DateTime'] = df_mean['DateTime']  # добавляем столбец для объединения в 5сек интервалы
+        stat_data_mid = df_mean.resample('1T', on='_DateTime').max()  # заполняем интервалы реальными значениями
+        # <-- конец, формируем таблицу с поминутными средними значениями, где 0-значения не работающего сервиса
 
         # формируем второй график, из ранее подготовленных данных, в виде гистограммы
         plot2 = px.bar(
-            df_plot2,
+            stat_data_mid,
             title='Активность ЦП за последний час, среднее за 1 мин.',
             x="DateTime",
             y="Loading, %",
-            range_x = [filter_dt_start, filter_dt_finish]
+            range_x=[filter_dt_start, filter_dt_finish]
         )
-        plot2.update_traces(marker_color='#00CC66')
+        plot2.update_traces(marker_color='#00CC66')  # указывам цвет столбцов графика
 
         # создаем HTML-код для отображения первого графика, с помощью plotly.offline.plot()
         t_plot_moment = plot(plot1, output_type="div")
@@ -119,3 +112,4 @@ def description(request):
 # возвращаем страницу ошибки
 def error(request):
     return render(request, 'main/error.html')
+
